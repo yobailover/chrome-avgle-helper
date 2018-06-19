df93415238476cd0e53ed479731805e02063b778
Liu Yue <hangxingliu@gmail.com>
2018-06-20 06:35:49 +0800

    fix video page pattern

--- src/config.js
+++ src/config.js
@@ -8,4 +8,4 @@ export const M3U8_PATTERN = '*://*.ahcdn.com/*.m3u8';
  * URL pattern of Avgle video player page
  * Avgle 视频播放页面的 URL Pattern
  */
-export const VIDEO_PAGE_PATTERN = /(https|http):\/\/avgle.com\/video\/\w+\//;
+export const VIDEO_PAGE_PATTERN = /(https|http):\/\/avgle.com\/video\/\w+/;
