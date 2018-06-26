0072a06f4b906091fa758215257ac19c9f4c7175
Liu Yue <hangxingliu@gmail.com>
2018-06-27 01:48:51 +0800

    1.3.1 support more car number

--- src/inject_utils.js
+++ src/inject_utils.js
@@ -4,7 +4,7 @@ let exportFunctions = {
 };
 export default exportFunctions;
 
-function getInjectCodes(wrapperName) { 
+function getInjectCodes(wrapperName) {
 	let result = `window.${wrapperName} = {};`;
 	result += Object.keys(exportFunctions).map(funcName => {
 		let funcBody = exportFunctions[funcName].toString();
@@ -13,32 +13,35 @@ function getInjectCodes(wrapperName) {
 	return result;
 }
 
-function parseCarNumber(str = '') { 
+function parseCarNumber(str = '') {
 	const matchers = [
+		//LOVE series ...
+		[/LOVE[-\s](\d{3,4})/i, match => `LOVE-${match[1]}`],
+
 		//10musume 123117_01 ...
-		[/10musume[\-_\s](\d{6})[\-_\s](\d{2})/i, match => `10musume-${match[1]}-${match[2]}`],
+		[/10musume[-_\s](\d{6})[-_\s](\d{2})/i, match => `10musume-${match[1]}-${match[2]}`],
 
 		// fc2-ppv-10045 or fc2ppv-10056 ...
-		[/fc2[\s\-_]?ppv[\s\-_]?(\d+)/i, match => `FC2-PPV-${match[1]}` ], 
-		
+		[/fc2[\s\-_]?ppv[\s\-_]?(\d+)/i, match => `FC2-PPV-${match[1]}` ],
+
 		// S-Cute Ava #1
 		[/S-Cute\s+(\w+)\s+#(\d+)/i, match => `S-Cute-${match[1]}-${match[2]}`],
 
 		//Heydouga
-		[/Heydouga[\-_\s]?(\d+)[\-_\s]?(\d+)/i, match => `Heydouga-${match[1]}-${match[2]}`],
+		[/Heydouga[-_\s]?(\d+)[-_\s]?(\d+)/i, match => `Heydouga-${match[1]}-${match[2]}`],
 
 		//heyzo
-		[/heyzo[\-_\s]?(\d+)/i, match => `heyzo-${match[1]}`],
+		[/heyzo[-_\s]?(\d+)/i, match => `heyzo-${match[1]}`],
 
 		//Caribbean
-		[/(\d+)[\-_\s](\d+)[\-_\s]Carib(?:bean(?:com)?)?/i, match => `carib-${match[1]}-${match[2]}`],
-		[/Carib(?:bean(?:com)?)?[\-_\s]?(\d+)[\-_\s]?(\d+)/i, match => `carib-${match[1]}-${match[2]}`],
+		[/(\d+)[-_\s](\d+)[-_\s]Carib(?:bean(?:com)?)?/i, match => `carib-${match[1]}-${match[2]}`],
+		[/Carib(?:bean(?:com)?)?[-_\s]?(\d+)[-_\s]?(\d+)/i, match => `carib-${match[1]}-${match[2]}`],
 
 		//xxx-010 ...
 		/\w+-\d+/i,
 
 		//xxxxx-01223-123
-		[/(\w{8,})[\-_\s](\d{5,})[\-_\s](\d{3,})/, match => `${match[1]}-${match[2]}-${match[3]}`]
+		[/(\w{8,})[-_\s](\d{5,})[-_\s](\d{3,})/, match => `${match[1]}-${match[2]}-${match[3]}`]
 	];
 
 	for (let matcher of matchers) {
@@ -46,7 +49,7 @@ function parseCarNumber(str = '') {
 		if (Array.isArray(matcher)) {
 			let result = str.match(matcher[0]);
 			if (result)
-				return matcher[1](result);	
+				return matcher[1](result);
 		} else {
 			let result = str.match(matcher);
 			if (result)
